1d4428d569cff5ed62187a9be8002439d5de3327
Jok3rTwin2-DEV <54908247+Jok3rTwin2-DEV@users.noreply.github.com>
2020-04-10 10:03:58 -0400

    Update game.c

--- game.c
+++ game.c
@@ -1,10 +1,18 @@
-//prints the title of the game
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+main()
+{
+  //prints title and dedication of the game 
+  printf("qqqqq              ");
+  printf("q   q              ");
+  printf("qqqqq              ");
+  printf("    q              ");
+  printf("    q              ");
+  printf("i dedicate this game to my baby seranity!");
+
+  return 0;
+}
 
 
-printf("questionz");
-printf("qqqqq");
-printf("q   q");
-printf("qqqqq");
-printf("    q");
-printf("    q");
-printf("i dedicate this game to my baby seranity!");
