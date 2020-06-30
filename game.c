dbc44acfad7810cb5328e9318470165b53d085d9
Jok3rTwin2-DEV <54908247+Jok3rTwin2-DEV@users.noreply.github.com>
2020-06-30 01:34:47 -0400

    Update game.c

--- game.c
+++ game.c
@@ -5,12 +5,14 @@
 main()
 {
   //prints title and dedication of the game 
-  printf("qqqqq              ");
-  printf("q   q              ");
-  printf("qqqqq              ");
-  printf("    q              ");
-  printf("    q              ");
+  printf("qqqqq u   u eeeee ssss ");
+  printf("q   q u   u e          ");
+  printf("qqqqq u   u eeeee      ");
+  printf("    q u   u e          ");
+  printf("    q uuuuu eeeee      ");
   printf("i dedicate this game to my baby seranity!");
+
+
 
   return 0;
 }
