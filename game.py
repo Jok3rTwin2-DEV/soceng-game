843b8df91af53e3ccf0d3d278c249ad77e7448f8
Jok3rTwin2-DEV <54908247+Jok3rTwin2-DEV@users.noreply.github.com>
2020-10-11 16:18:44 +0000

    Add files via upload

---
+++ game.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+#imports
+import os
+import subprocess
+import smtplib
+'''
+Project DEV: Ryan J Wilke
+Github: Https://github.com/Jok3rTwin2-DEV
+Email: Jok3rtwin2@protonmail.com
+
+'''
+
+#this is the intro to my program
+def title():                               	
+	print('\nI dedicate this program to sara\n')
+	print('There is a surprise at the end play the whole game to find out ;)' )
+	print('NOW LETS BEGIN')
+
+title()	
+
+#now lets start the program
+#first question
+def question1():
+			
+	x = input('\n\nwhats your first name: ')
+	y = input('whats your last name: ')
+	text = x
+	text = y
+	saveFile = open('answers.txt', 'w')
+	saveFile.write(text)
+	saveFile.close
+		
+question1()
+
+def question2():
+	x = input('whats your childhood nickname: ')
+	text = x 
+	saveFile = open('answers.txt', 'w')
+	saveFile.write(text)
+	saveFile.close
+	
+question2()
+
+#third question
+def question3():
+	x = input('whats your date of birth (MM,DD,YYYY): ')
+	text = x 
+	saveFile = open('answers.txt', 'a')
+	saveFile.write(text)
+	saveFile.close()
+	
+question3()
+
+#third question
+def question4():
+	x = input('\ndo you have a pet?: ')
+	text = x 
+	saveFile = open('answers.txt', 'a')
+	saveFile.write(text)
+	saveFile.close()
+
+question4()
+
+
+#prints answers out onto screen
+def answers_output():
+		x = input('click enter to finish....')
+		readMe = open('answers.txt', 'r') .read()
+		print(readMe) 
+
+answers_output()
+
