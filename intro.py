441addb17e480b9a5f48a7ea9f8a858deafa1991
Jok3rTwin2-DEV <54908247+Jok3rTwin2-DEV@users.noreply.github.com>
2020-10-11 16:17:52 +0000

    Delete intro.py

--- intro.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def title():                               	
-	print('\nI dedicate this program to sara\n')
-	print('There is a surprise at the end play the whole game to find out ;)' )
-	print('NOW LETS BEGIN')
-
-title()	
